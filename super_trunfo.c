787bcd59f501a482a21f6ebc9484ee46396d560d
Hizaron <71935787+EvandroPC6@users.noreply.github.com>
2025-03-20 20:45:49 +0000

    push1

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,7 +2,7 @@
 
 int main(){
     char estado1;
-    char codigo1[2];
+    char codigo1[4];
     char nomeCidade1[15];
     int populacao1; 
     int pontosTuristicos1;
@@ -30,6 +30,11 @@ int main(){
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
-    printf("Carta1:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.1fkm²\nPIB: %.2fbilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado1, codigo1, nomeCidade1, populacao1, area1, pib1, pontosTuristicos1);
-
+    printf("Carta1:\nEstado: %c\n", estado1);
+    printf("Código: %c%s\n", estado1, codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.1fkm²\n", area1);
+    printf("PIB: %.2fbilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 }
